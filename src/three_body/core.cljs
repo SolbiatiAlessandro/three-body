76a8ce4cc1c00a01e2ab09a1a81292fe68d792e1
Alessandro Solbiati <lessandro@fb.com>
2020-11-14 19:00:52 +0000

    [cljs][2/n] physics, compute accellerations

--- src/three_body/core.cljs
+++ src/three_body/core.cljs
@@ -6,8 +6,10 @@
 (defrecord Body [id x y vx vy ax ay])
 (defn rand-x [] (+ 3 (rand-int 94)))
 (defn body [id] (Body. id (rand-x) (rand-x) 0 0 0 0))
-(def bodies (map (fn [id] (body (+ 1 id))) (range 3)))
+(defn init-bodies [n] (map (fn [id] (body (+ 1 id))) (range n)))
 (def comb [[1 2] [0 2] [0 1]])
+(def bodies (atom (init-bodies 3)))
+(declare render)
 
 ;; PHYSICS
 
@@ -27,36 +29,40 @@
         magnitude (/ G (* distance distance))]
     [(* magnitude (js/Math.cos angle)) (* magnitude (js/Math.sin angle))]))
 
+(defn vector-sum [vectors]
+  (reduce (fn [acc x] [(+ (first acc) (first x)) (+ (second acc) (second x))]) vectors))
+
 (defn g-accell 
-  
- ([i bodies dt] 
+ ([i bodies] 
   "Compute accelleration for i-th body"
-
-  (let [force-components (map 
+  (let [acc-components (map 
                            (fn [j] (g-force (nth bodies i) (nth bodies j)))
-                           (nth comb i))])
+                           (nth comb i))]
+    (vector-sum acc-components))) 
+ ([bodies]
+  "Return bodies with updated accellerations"
+   (map-indexed (fn [i body] (let [[ax ay] (g-accell i bodies)]
+                  (assoc body :ax ax :ay ay)))
+                bodies )))
 
-  ;; TODO: add force compoonents with reduce
-  ;; (reduce + [[1 2] [3 4] [4 5]]) --> [8 11]
-  
-  ) 
-  ([bodies dt]
-   "Compute accelleration for all the bodies"
-
-   (
-    ))
-  
-  )
+(defn step-physics-simulation [bodies ts]
+  (let [bodies  (g-accell bodies)]
+    (render bodies)
+    bodies))
 
 ;; RENDERING
 
-(defn render 
-  ([] (doseq [body bodies] (render @body)))
-  ([body] (let [style (.-style (.getElementById js/document (str "body" (:id body))))]
-    (set! (.-bottom style) (str (:y body) "%"))
-    (set! (.-left style) (str (:x body) "%")))))
+(defn render [bodies] 
+  (doseq [body bodies] 
+    (let [style (.-style (.getElementById js/document (str "body" (:id body))))]
+      (set! (.-bottom style) (str (:y body) "%"))
+      (set! (.-left style) (str (:x body) "%")))))
 
-(defn main []
-  (render))
+(defn main [ts]
+  (do
+   (js/window.requestAnimationFrame main)  
+   (swap! bodies step-physics-simulation ts)
+   ;; (js/console.log (clj->js bodies))
+   ))
 
-(main)
+(main 0)
